e6ff93d0ea9d06cbefefd1f6c7a9c5f63696b3ee
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2021-11-20 09:28:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function lunar_eclipse(){
+    document.getElementById("eclipse_img").src="lunar.jpg";
+    document.getElementById("eclipse_text").innerHTML="Lunar Eclipse"
+}
+    
+function solar_eclipse(){
+    document.getElementById("eclipse_img").src="solar.jpg";
+    document.getElementById("eclipse_text").innerHTML="Solar Eclipse"
+}
+    
